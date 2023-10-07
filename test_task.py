40b0a301b5b9a157baf12d0a7b278e6e624cfdab
Chenchen Liu <chenchen.liu@u.nus.edu>
2023-10-07 21:31:07 +0800

    modify test_task

--- test_task.py
+++ test_task.py
@@ -1,19 +1,14 @@
+import numpy as np
 from panda_gym.pybullet import PyBullet
 from panda_gym.envs.tasks.reach_cam import ReachCam
-from panda_gym.envs.tasks.slide import Slide
+from panda_gym.envs.robots.panda_camera import PandaWithCamera
+
 sim = PyBullet(render_mode="rgb_array")
+robot = PandaWithCamera(sim, block_gripper=True, base_position=np.array([-0.6, 0.0, 0.0]), control_type="joints")
+task1= ReachCam(sim, get_ee_position=robot.get_ee_position)
 
-task2 = ReachCam(sim)
-
-# task1 = Slide(sim)
-# task1.reset()
-# print(task1.get_obs())
-# print(task1.get_achieved_goal())
-# print(task1.is_success(task1.get_achieved_goal(), task1.get_goal()))
-# print(task1.compute_reward(task1.get_achieved_goal(), task1.get_goal()))
-# print("----------------------------------------------------------------")
-task2.reset()
-for _ in range(1000):
-    print(task2.get_obs())
-# print(task2.get_achieved_goal())
-# print(task2.get_goal())
+task1.reset()
+print(task1.get_obs())
+print(task1.get_achieved_goal())
+print(task1.is_success(task1.get_achieved_goal(), task1.get_goal()))
+print(task1.compute_reward(task1.get_achieved_goal(), task1.get_goal(), {}))
