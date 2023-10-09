282e1ac3ea1e53a6fd049abadf6584c064afb225
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-10-09 15:05:05 +0800

    Change to gym.make

--- test_sb3.py
+++ test_sb3.py
@@ -1,7 +1,9 @@
 from panda_gym.envs import PandaReachCamEnv
 from stable_baselines3 import DDPG, HerReplayBuffer
 
-env = PandaReachCamEnv(render_mode="human") # rgb_array
+import gymnasium as gym
+
+env = gym.make('PandaReachCam-v3', render_mode="human") # rgb_array
 
 model = DDPG(policy="MultiInputPolicy", env=env, replay_buffer_class=HerReplayBuffer, verbose=1, buffer_size=300000, tensorboard_log="./tmp")
 
