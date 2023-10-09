416fdbf7331e7f235fbae3ce19c82b6c753c2641
Chenchen Liu <chenchen.liu@u.nus.edu>
2023-10-09 16:06:57 +0800

    delete stationary cam in task and remove stationary camera in panda_cam

--- panda_gym/envs/tasks/reach_cam.py
+++ panda_gym/envs/tasks/reach_cam.py
@@ -63,8 +63,8 @@ class ReachCam(Task):
         """
         Stationary camera that is directly in front of the robot arm
         """
-        cam_pos = self.sim.get_link_position("panda_camera", self.staionary_cam_link)
-        cam_orn = self.sim.get_link_orientation("panda_camera", self.staionary_cam_link)
+        cam_pos = self.sim.get_link_position("stationary_camera", self.staionary_cam_link)
+        cam_orn = self.sim.get_link_orientation("stationary_camera", self.staionary_cam_link)
         cam_pos[0] = cam_pos[0] - 0.0115*math.cos(math.pi/4) -0.001 # 11.5 mm is half of D405 cam thickness, but need to use trigonometry because the camera is rotated 45 deg
         cam_pos[2] = cam_pos[2] - 0.0115*math.sin(math.pi/4) - 0.001
         rot_matrix = np.array(self.sim.physics_client.getMatrixFromQuaternion(cam_orn)).reshape(3,3) # 3x3 rotation matrix (right, forward, up by columns)
