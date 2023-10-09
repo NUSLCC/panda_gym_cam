1682677467721751aa8aa0197de786fc9b87e199
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-10-09 19:41:23 +0800

    Fixed robot-cam rendering

--- test_both_cam.py
+++ test_both_cam.py
@@ -22,11 +22,11 @@ for i in range(10000):
     """
     Robot cam block below
     """
-    camera_state = p.getLinkState(pandaId, 12)
+    camera_state = p.getLinkState(pandaId, 13)
    # print(f"Camera state is {camera_state}")
     camera_pos = np.array(camera_state[0])
-    camera_pos[2] = camera_pos[2] - 0.0185 # 16 mm is half of D405 cam thickness, but need to extend 18.5mm out to not see cam material itself
     camera_orn = np.array(camera_state[1])
+  #  print(f"Camera orn: {p.getEulerFromQuaternion(camera_orn)}")
     rot_matrix = np.array(p.getMatrixFromQuaternion(camera_orn)).reshape(3,3) # 3x3 rotation matrix (right, forward, up by columns)
     forward_vec = rot_matrix.dot(np.array((0, 0, -1)))
     up_vec = rot_matrix.dot(np.array((0, 1, 0)))
@@ -38,7 +38,8 @@ for i in range(10000):
     """
     camera_state2 = p.getLinkState(camId, 1)
     camera_pos2 = np.array(camera_state2[0])
-    camera_pos2[2] = camera_pos2[2] - 0.0185 # 16 mm is half of D405 cam thickness, but need to extend 18.5mm out to not see cam material itself
+    camera_pos2[0] = camera_pos2[0] - 0.0115*math.cos(math.pi/4) -0.001 # 11.5 mm is half of D405 cam thickness, but need to use trigonometry because the camera is rotated 45 deg
+    camera_pos2[2] = camera_pos2[2] - 0.0115*math.sin(math.pi/4) - 0.001
     camera_orn2 = np.array(camera_state2[1])
 
     rot_matrix2 = np.array(p.getMatrixFromQuaternion(camera_orn2)).reshape(3,3) # 3x3 rotation matrix (right, forward, up by columns)
