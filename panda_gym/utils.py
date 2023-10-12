ff9d56ac6f0061ada39b728fe8192a72a9fd0798
Chenchen Liu <chenchen.liu@u.nus.edu>
2023-10-12 19:42:08 +0800

    change range of sphere

--- panda_gym/utils.py
+++ panda_gym/utils.py
@@ -128,11 +128,10 @@ def generate_object_range(initial_x_coord, initial_y_coord, initial_z_coord):
     """
     horiz_total_dis = 2*initial_z_coord*math.tan(math.radians(87)/2) 
     vert_total_dis = 2*initial_z_coord*math.tan(math.radians(58)/2)
-    buffer = 0.2
-    x_min = initial_x_coord - vert_total_dis/2 - buffer
-    x_max = initial_x_coord + vert_total_dis/2 + buffer
-    y_min = initial_y_coord - horiz_total_dis/2 - buffer
-    y_max = initial_y_coord + horiz_total_dis/2 + buffer
+    x_min = initial_x_coord - vert_total_dis/2 - 0.5
+    x_max = initial_x_coord + vert_total_dis/2 + 0.2
+    y_min = initial_y_coord - horiz_total_dis/2 - 0.3
+    y_max = initial_y_coord + horiz_total_dis/2 + 0.3
 
     # Calculate obj_range_low and obj_range_high - they form the bounding box where the object can be randomly generated
     obj_range_low = np.array([x_min, y_min, 0])
