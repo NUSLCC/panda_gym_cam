2209c14d61f7be4f78f1d39be01d2b52b9cdb6cc
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-11-24 21:19:19 +0800

    Change learning starts back to 100

--- test_sac_her.py
+++ test_sac_her.py
@@ -13,7 +13,7 @@ num_cpu = 4
 env = make_vec_env(env_id, n_envs=num_cpu, seed=0)
 
 model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
-            train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=500,
+            train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=100,
             buffer_size=300000, replay_buffer_class=HerReplayBuffer,
             # Parameters for HER
             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
