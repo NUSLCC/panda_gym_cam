5c75967624dd627dfc473120ba908a2e7ccfaa08
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-11-28 15:37:57 +0800

    add enough timesteps and multi cpu

--- test_sac_her.py
+++ test_sac_her.py
@@ -9,7 +9,7 @@ import gymnasium as gym
 if __name__=="__main__":
     # env = gym.make('PandaReachCam-v3', render_mode="human") #, control_type="joints") # rgb_array
     env_id = "PandaReachCamJoints-v3"
-    num_cpu = 8
+    num_cpu = 16
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
     model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
@@ -26,5 +26,5 @@ if __name__=="__main__":
     new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
     model.set_logger(new_logger)
 
-    model.learn(total_timesteps=500_000, progress_bar=True)
+    model.learn(total_timesteps=700_000, progress_bar=True)
     model.save("sac_her_panda")
