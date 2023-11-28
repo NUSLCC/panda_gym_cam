f643fb6340a0c819d13d1167360c915957c12f2d
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-11-28 20:40:49 +0800

    Update test_sac file

--- test_sac_her.py
+++ test_sac_her.py
@@ -7,24 +7,25 @@ from datetime import datetime
 
 import gymnasium as gym
 
-# env = gym.make('PandaReachCam-v3', render_mode="human") #, control_type="joints") # rgb_array
-env_id = "PandaReachCamJoints-v3"
-num_cpu = 4
-env = make_vec_env(env_id, n_envs=num_cpu, seed=0)
+if __name__ == "__main__":
+    # env = gym.make('PandaReachCam-v3', render_mode="human") #, control_type="joints") # rgb_array
+    env_id = "PandaReachCamJoints-v3"
+    num_cpu = 16
+    env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
-model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
-            train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=100,
-            buffer_size=300000, replay_buffer_class=HerReplayBuffer,
-            # Parameters for HER
-            replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
-            # Parameters for SAC
-            policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2))
+    model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
+                train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1000,
+                buffer_size=600000, replay_buffer_class=HerReplayBuffer,
+                # Parameters for HER
+                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
+                # Parameters for SAC
+                policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2))
 
 
-tmp_path = "./tmp/"+datetime.now().strftime('sac_single_table_%H_%M_%d')
-# set up logger
-new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-model.set_logger(new_logger)
+    tmp_path = "./tmp/"+datetime.now().strftime('sac_single_table_%H_%M_%d')
+    # set up logger
+    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    model.set_logger(new_logger)
 
-model.learn(total_timesteps=100_000, progress_bar=True)
-model.save("sac_her_panda")
+    model.learn(total_timesteps=700_000, progress_bar=True)
+    model.save("sac_her_panda")
