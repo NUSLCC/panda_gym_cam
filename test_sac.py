6c582ff3fd6d860b4bfb276c021cf27bb9c165c3
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-11-29 20:41:32 +0800

    Update sac

--- test_sac.py
+++ test_sac.py
@@ -1,5 +1,6 @@
 from panda_gym.envs import PandaReachCamEnv
-from stable_baselines3 import SAC, HerReplayBuffer
+from stable_baselines3 import SAC
+from stable_baselines3.common.buffers import DictReplayBuffer
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.logger import configure
@@ -7,24 +8,21 @@ from datetime import datetime
 import gymnasium as gym
 
 if __name__=="__main__":
-    # env = gym.make('PandaReachCam-v3', render_mode="human") #, control_type="joints") # rgb_array
     env_id = "PandaReachCamJoints-v3"
     num_cpu = 16
-    env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
+    env = make_vec_env('PandaReachCam-v3', n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
     model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
                 train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1000,
-                buffer_size=600000, replay_buffer_class=HerReplayBuffer,
-                # Parameters for HER
-                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
+                buffer_size=600000, replay_buffer_class=DictReplayBuffer,
                 # Parameters for SAC
                 policy_kwargs=dict(net_arch=[512, 512, 512], n_critics=2))
 
 
-    tmp_path = "./tmp/"+datetime.now().strftime('sac_dual_table_%H_%M_%d')
+    tmp_path = "./tmp/"+datetime.now().strftime('sac_dual_table_only_vision_%H_%M_%d')
     # set up logger
     new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
     model.set_logger(new_logger)
 
     model.learn(total_timesteps=700_000, progress_bar=True)
-    model.save("sac_her_panda")
+    model.save("sac_dual_only_vision_panda")
