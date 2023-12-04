4ad962a70bb87e03d5e088a171b877ddfa96baf4
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-12-04 19:32:55 +0800

    Changed to customcombinedextractor

--- test_sac_her.py
+++ test_sac_her.py
@@ -1,12 +1,12 @@
 from panda_gym.envs import PandaReachCamEnv
-from stable_baselines3 import SAC
-from stable_baselines3.common.buffers import ReplayBuffer
+from stable_baselines3 import SAC, HerReplayBuffer
+from stable_baselines3.common.buffers import ReplayBuffer, DictReplayBuffer
 from stable_baselines3.common.vec_env import DummyVecEnv, SubprocVecEnv
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.logger import configure
 from datetime import datetime
 import gymnasium as gym
-from attention.modules import CustomFeatureExtractor
+from attention.modules import CustomFeatureExtractor, CustomCombinedExtractor
 
 if __name__=="__main__":
     # env = gym.make('PandaReachCam-v3', render_mode="human") #, control_type="joints") # rgb_array
@@ -14,12 +14,13 @@ if __name__=="__main__":
     num_cpu = 16
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
-    model = SAC(policy="CnnPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
+    model = SAC(policy="MultiInputPolicy",env=env, batch_size=128, gamma=0.95, learning_rate=1e-4, verbose=1, 
                 train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1000,
-                buffer_size=600000, replay_buffer_class=ReplayBuffer,
+                buffer_size=600000, replay_buffer_class=DictReplayBuffer,
+                # Parameters for SAC
                 policy_kwargs=dict(
-                    features_extractor_class=CustomFeatureExtractor,
-                    features_extractor_kwargs=dict(features_dim=76608), # output size of custom feature extractor
+                    features_extractor_class=CustomCombinedExtractor,
+                    features_extractor_kwargs=dict(features_dim=30528), # output size of custom combined extractor, e.g. Identity projection input/output: torch.Size([16, 30528])
                     net_arch=[512, 512, 512], 
                     n_critics=2)
     )
@@ -30,4 +31,4 @@ if __name__=="__main__":
     model.set_logger(new_logger)
 
     model.learn(total_timesteps=700_000, progress_bar=True)
-    model.save("sac_dual_attention_panda")
+    model.save("sac_single_attention_panda")
