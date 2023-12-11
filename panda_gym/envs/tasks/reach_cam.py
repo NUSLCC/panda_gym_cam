c08fb3cc6253c8b4434cee089a15354f5eee55ac
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-12-11 20:01:23 +0800

    Added color jitter

--- panda_gym/envs/tasks/reach_cam.py
+++ panda_gym/envs/tasks/reach_cam.py
@@ -15,6 +15,7 @@ from panda_gym.utils import masked_auto_encoder
 from panda_gym.utils import velocity_calculator
 from panda_gym.utils import sine_velocity
 
+
 class ReachCam(Task):
     def __init__(
         self,
@@ -28,10 +29,10 @@ class ReachCam(Task):
         super().__init__(sim)
         self.reward_type = reward_type
         self.image_overlap_threshold = image_overlap_threshold
-        self.distance_threshold=distance_threshold
+        self.distance_threshold = distance_threshold
         self.far_distance_threshold = 1.0
         self.object_size = 0.04
-        self.object_velocity_max = [0.15, 0.15, 0] # (x,y,z) velocity 
+        self.object_velocity_max = [0.15, 0.15, 0]  # (x,y,z) velocity
         self.get_ee_position = get_ee_position
         self.goal_range_low = None
         self.goal_range_high = None
@@ -47,9 +48,9 @@ class ReachCam(Task):
         self.sim.create_plane(z_offset=-0.4)
         self.sim.create_box(
             body_name="black_panda_table",
-            half_extents=np.array([0.32, 0.32, 0.398/2]),
+            half_extents=np.array([0.32, 0.32, 0.398 / 2]),
             mass=0.0,
-            position=np.array([-0.68, 0, -0.398/2]),
+            position=np.array([-0.68, 0, -0.398 / 2]),
             rgba_color=np.array([0, 0, 0, 1]),
         )
         self.sim.create_box(
@@ -57,30 +58,32 @@ class ReachCam(Task):
             half_extents=np.array([0.2, 0.2, 0.01]),
             mass=0.0,
             position=np.array([-0.68, 0, -0.0001]),
-            rgba_color=np.array([192/255, 192/255, 192/255, 1]),
+            rgba_color=np.array([192 / 255, 192 / 255, 192 / 255, 1]),
         )
         self.sim.create_box(
             body_name="white_table",
-            half_extents=np.array([0.4, 0.64, 0.398/2]), 
+            half_extents=np.array([0.4, 0.64, 0.398 / 2]),
             mass=0.0,
-            position=np.array([0.04, 0, -0.398/2]),
+            position=np.array([0.04, 0, -0.398 / 2]),
             rgba_color=np.array([1, 1, 1, 1]),
         )
         self.sim.create_sphere(
             body_name="target",
-            radius=self.object_size/2,
+            radius=self.object_size / 2,
             mass=0.0,
             ghost=True,
             position=np.array([0.0, 0.0, self.object_size / 2]),
             rgba_color=np.array([0.1, 0.9, 0.1, 1]),
         )
-        self.sim.loadURDF( 
+        self.sim.loadURDF(
             body_name="stationary_camera",
             fileName="URDF_files/L515_cam_with_stand.urdf",
-            basePosition=[0.65, 0, 0.5-0.3],
+            basePosition=[0.65, 0, 0.5 - 0.3],
             useFixedBase=True,
         )
-        self.object_initial_velocity = np.random.uniform(np.array(self.object_velocity_max) / 2, self.object_velocity_max)
+        self.object_initial_velocity = np.random.uniform(
+            np.array(self.object_velocity_max) / 2, self.object_velocity_max
+        )
 
         # self.sim.create_sphere(
         #     body_name="outer",
@@ -116,8 +119,11 @@ class ReachCam(Task):
         # )
 
     def get_obs(self) -> np.ndarray:
-        rgb_img = self.render_from_stationary_cam() 
-        # jittered_img = colorjitter(rgb_img, brightness = 0.5, contrast = 0.5, saturation = 0.5, hue = 0.3)
+        rgb_img = self.render_from_stationary_cam()
+        jittered_img = colorjitter(
+            rgb_img, brightness=0.5, contrast=0.5, saturation=0.5, hue=0.5
+        )
+        return jittered_img
         # mae_img = masked_auto_encoder(jittered_img)
         # return mae_img
         # target_position = self.sim.get_base_position("target")
@@ -125,7 +131,6 @@ class ReachCam(Task):
         # self.object_initial_velocity = velocity_calculator(target_position, np.array(self.object_initial_velocity))
         # target_velocity = self.object_initial_velocity
         # self.sim.set_base_velocity("target", target_velocity)
-        return rgb_img
 
     def render_from_stationary_cam(
         self,
@@ -137,21 +142,45 @@ class ReachCam(Task):
         """
         Stationary camera that is directly in front of the robot arm
         """
-        cam_pos = self.sim.get_link_position("stationary_camera", self.stationary_cam_link)
-        cam_orn = np.array(p.getQuaternionFromEuler([math.radians(90-self.stationary_cam_pitch_angle), 0, math.pi/2]))
-        cam_pos[0] = cam_pos[0] - 0.013*math.cos(math.radians(90-self.stationary_cam_pitch_angle)) # 13 mm is half of L515 cam thickness, but need to use trigonometry because the camera is rotated 45 deg
-        cam_pos[2] = cam_pos[2] - 0.013*math.sin(math.radians(90-self.stationary_cam_pitch_angle)) 
-        rot_matrix = np.array(self.sim.physics_client.getMatrixFromQuaternion(cam_orn)).reshape(3,3) # 3x3 rotation matrix (right, forward, up by columns)
+        cam_pos = self.sim.get_link_position(
+            "stationary_camera", self.stationary_cam_link
+        )
+        cam_orn = np.array(
+            p.getQuaternionFromEuler(
+                [math.radians(90 - self.stationary_cam_pitch_angle), 0, math.pi / 2]
+            )
+        )
+        cam_pos[0] = cam_pos[0] - 0.013 * math.cos(
+            math.radians(90 - self.stationary_cam_pitch_angle)
+        )  # 13 mm is half of L515 cam thickness, but need to use trigonometry because the camera is rotated 45 deg
+        cam_pos[2] = cam_pos[2] - 0.013 * math.sin(
+            math.radians(90 - self.stationary_cam_pitch_angle)
+        )
+        rot_matrix = np.array(
+            self.sim.physics_client.getMatrixFromQuaternion(cam_orn)
+        ).reshape(
+            3, 3
+        )  # 3x3 rotation matrix (right, forward, up by columns)
         forward_vec = rot_matrix.dot(np.array((0, 0, -1)))
         up_vec = rot_matrix.dot(np.array((0, 1, 0)))
         target_position = cam_pos + 0.1 * forward_vec
-        view_matrix = self.sim.physics_client.computeViewMatrix(cam_pos, target_position, up_vec)
+        view_matrix = self.sim.physics_client.computeViewMatrix(
+            cam_pos, target_position, up_vec
+        )
         aspect_ratio = cam_width / cam_height
         fov = 43
         nearVal = 0.01
         farVal = 100
-        proj_matrix = self.sim.physics_client.computeProjectionMatrixFOV(fov, aspect_ratio, nearVal, farVal)
-        rgb_img = self.sim.physics_client.getCameraImage(cam_width, cam_height, view_matrix, proj_matrix, renderer = p.ER_BULLET_HARDWARE_OPENGL)[2]
+        proj_matrix = self.sim.physics_client.computeProjectionMatrixFOV(
+            fov, aspect_ratio, nearVal, farVal
+        )
+        rgb_img = self.sim.physics_client.getCameraImage(
+            cam_width,
+            cam_height,
+            view_matrix,
+            proj_matrix,
+            renderer=p.ER_BULLET_HARDWARE_OPENGL,
+        )[2]
         rgb_img = np.array(rgb_img).reshape(cam_width, cam_height, 4)[:, :, :3]
         return rgb_img
 
@@ -160,33 +189,50 @@ class ReachCam(Task):
         return ee_position
 
     def reset(self) -> None:
-        self.robot_cam_initial_x, self.robot_cam_initial_y, self.robot_cam_initial_z = self.sim.get_link_position("panda_camera", self.cam_link)
+        (
+            self.robot_cam_initial_x,
+            self.robot_cam_initial_y,
+            self.robot_cam_initial_z,
+        ) = self.sim.get_link_position("panda_camera", self.cam_link)
         self.goal_range_low, self.goal_range_high = generate_semicircle_object_range()
         self.goal = self._sample_goal()
         self.sim.set_base_pose("target", self.goal, np.array([0.0, 0.0, 0.0, 1.0]))
-       # self.object_initial_velocity = np.random.uniform(np.array(self.object_velocity_max) / 2, self.object_velocity_max)
-      #  self.object_initial_velocity = np.array([0, 0.1, 0]) # for sin function 
+
+    # self.object_initial_velocity = np.random.uniform(np.array(self.object_velocity_max) / 2, self.object_velocity_max)
+    #  self.object_initial_velocity = np.array([0, 0.1, 0]) # for sin function
 
     def _sample_goal(self) -> np.ndarray:
         """Randomize goal."""
-        goal = np.array([0.0, 0.0, self.object_size / 2])  # z offset for the sphere center
+        goal = np.array(
+            [0.0, 0.0, self.object_size / 2]
+        )  # z offset for the sphere center
         noise = np.random.uniform(self.goal_range_low, self.goal_range_high)
         goal += noise
         return goal
 
-    def is_success(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> np.ndarray:
+    def is_success(
+        self, achieved_goal: np.ndarray, desired_goal: np.ndarray
+    ) -> np.ndarray:
         d = distance(achieved_goal, desired_goal)
         return np.array(d < self.distance_threshold, dtype=bool)
 
-    def is_terminated(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> np.ndarray:
+    def is_terminated(
+        self, achieved_goal: np.ndarray, desired_goal: np.ndarray
+    ) -> np.ndarray:
         d = distance(achieved_goal, desired_goal)
-        return np.array(d < self.distance_threshold or d > self.far_distance_threshold, dtype=bool)
+        return np.array(
+            d < self.distance_threshold or d > self.far_distance_threshold, dtype=bool
+        )
 
-    def is_failure(self, achieved_goal: np.ndarray, desired_goal: np.ndarray) -> np.ndarray:
+    def is_failure(
+        self, achieved_goal: np.ndarray, desired_goal: np.ndarray
+    ) -> np.ndarray:
         d = distance(achieved_goal, desired_goal)
         return np.array(d > self.far_distance_threshold, dtype=bool)
 
-    def compute_reward(self, achieved_goal, desired_goal, info: Dict[str, Any]) -> np.ndarray:
+    def compute_reward(
+        self, achieved_goal, desired_goal, info: Dict[str, Any]
+    ) -> np.ndarray:
         d = distance(achieved_goal, desired_goal)
         if self.reward_type == "sparse":
             return -np.array(d > self.distance_threshold, dtype=np.float32)
