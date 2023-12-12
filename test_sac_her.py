c34b2be6eb561a643876f499fe480ed6ec633494
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-12-12 20:47:47 +0800

    have to increase learning start

--- test_sac_her.py
+++ test_sac_her.py
@@ -13,7 +13,7 @@ if __name__=="__main__":
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
     model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
-                train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1000,
+                train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=2000,
                 buffer_size=600000, replay_buffer_class=HerReplayBuffer, device="cuda:1",
                 # Parameters for HER
                 replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
