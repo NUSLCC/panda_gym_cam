a31f0904d93ea96deb2adc0d50afe0475ac80b8b
philiprxwang <philipruoxuan.wang@yahoo.com>
2023-12-25 17:39:44 +0800

    imshow original observation (this is ok)

--- test_load_query.py
+++ test_load_query.py
@@ -3,6 +3,7 @@ from stable_baselines3 import DDPG, SAC, HerReplayBuffer
 from sb3_contrib import TQC
 import time
 import gymnasium as gym
+import matplotlib.pyplot as plt
 
 env = gym.make('PandaReachCam-v3', render_mode="human", control_type="joints") # rgb_array
 # print(env.action_space)
@@ -20,6 +21,10 @@ obs, _ = env.reset()
 for i in range(1000):
     action, _states = model.predict(obs, deterministic=True)
     # print(action)
+    
     obs, _ = env.reset()
+
+    plt.imshow(obs["observation"].reshape(180,160,3))
+    plt.show()
     #obs, reward, terminated, truncated, info = env.step(action)
     env.render()
