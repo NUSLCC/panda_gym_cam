faa925beb5d2579e6a44cfd0ed44ea5fe3c3a9f0
philiprxwang <philipruoxuan.wang@yahoo.com>
2024-01-29 10:26:50 +0800

    Minor changes

--- test_sac_her.py
+++ test_sac_her.py
@@ -18,9 +18,9 @@ if __name__=="__main__":
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
     print(f'Action space: {env.action_space}')
     
-    # Save a checkpoint every 50000 steps
+    # Save a checkpoint every 100000 steps
     checkpoint_callback = CheckpointCallback(
-    save_freq=max(50000 // num_cpu, 1),
+    save_freq=max(100000 // num_cpu, 1),
     save_path="./logs/",
     name_prefix="philip4_pick_and_place",
     save_replay_buffer=True,
@@ -28,31 +28,31 @@ if __name__=="__main__":
     )
     
     # Training from scratch:
-    # model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
-    #             train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1500,
-    #             buffer_size=20000, replay_buffer_class=HerReplayBuffer, device="cuda:0",
-    #             # Parameters for HER
-    #             replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
-    #             # Parameters for SAC
-    #             policy_kwargs=dict(
-    #                 features_extractor_class=CustomCombinedExtractor,
-    #                 features_extractor_kwargs=dict(cnn_output_dim = 512),
-    #                 net_arch=[512, 512, 512], 
-    #                 n_critics=2)
-    #             )
-    # tmp_path = "./tmp/"+datetime.now().strftime('sac_dual_philip4_pickandplace_%H_%M_%d')
-    # new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-    # model.set_logger(new_logger)
-    # model.learn(total_timesteps=1_500_000, callback=checkpoint_callback, progress_bar=True)
+    model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
+                train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1500,
+                buffer_size=20000, replay_buffer_class=HerReplayBuffer, device="cuda:0",
+                # Parameters for HER
+                replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
+                # Parameters for SAC
+                policy_kwargs=dict(
+                    features_extractor_class=CustomCombinedExtractor,
+                    features_extractor_kwargs=dict(cnn_output_dim = 512),
+                    net_arch=[512, 512, 512], 
+                    n_critics=2)
+                )
+    tmp_path = "./tmp/"+datetime.now().strftime('sac_dual_philip4_pickandplace_%H_%M_%d')
+    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    model.set_logger(new_logger)
+    model.learn(total_timesteps=2_500_000, callback=checkpoint_callback, progress_bar=True)
 
     # Loading model:
 
-    model = SAC.load("logs/philip4_pick_and_place_1200000_steps", env = env)
-    model.load_replay_buffer("logs/philip4_pick_and_place_replay_buffer_1200000_steps", truncate_last_traj=False)
-    print(f'Replay buffer size is {model.replay_buffer.size()}')
-    tmp_path = "./tmp/"+"sac_dual_philip4_pickandplace_07_50_27"
-    new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
-    model.set_logger(new_logger)
-    model.learn(total_timesteps=1_500_000, callback=checkpoint_callback, reset_num_timesteps=False, progress_bar=True)
+    # model = SAC.load("logs/philip4_pick_and_place_1200000_steps", env = env)
+    # model.load_replay_buffer("logs/philip4_pick_and_place_replay_buffer_1200000_steps", truncate_last_traj=False)
+    # print(f'Replay buffer size is {model.replay_buffer.size()}')
+    # tmp_path = "./tmp/"+"sac_dual_philip4_pickandplace_07_50_27"
+    # new_logger = configure(tmp_path, ["stdout", "csv", "tensorboard"])
+    # model.set_logger(new_logger)
+    # model.learn(total_timesteps=1_500_000, callback=checkpoint_callback, reset_num_timesteps=False, progress_bar=True)
     
     model.save("sac_her_philip4_pickandplace")
