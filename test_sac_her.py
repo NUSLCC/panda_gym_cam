7a1bde322097f9a660cac41b3804f7665999eb66
Chenchen Liu <chenchen.liu@u.nus.edu>
2024-02-02 01:39:53 +0800

    change to JointDense and still not work

--- test_sac_her.py
+++ test_sac_her.py
@@ -10,13 +10,14 @@ import torch
 
 if __name__=="__main__":
     # env_id = "PandaReachCamJoints-v3"
-    env_id = "PandaPickAndPlaceCamJoints-v3"
-    num_cpu = 24
+    # env_id = "PandaPickAndPlaceCamJoints-v3"
+    env_id = "PandaPickAndPlaceCamJointsDense-v3"
+    num_cpu = 36
     env = make_vec_env(env_id, n_envs=num_cpu, seed=0, vec_env_cls=SubprocVecEnv)
 
     model = SAC(policy="MultiInputPolicy",env=env, batch_size=2048, gamma=0.95, learning_rate=1e-4, verbose=1, 
                 train_freq=64, gradient_steps=64, tau=0.05, tensorboard_log="./tmp", learning_starts=1500,
-                buffer_size=100000, replay_buffer_class=HerReplayBuffer, device="cuda:0",
+                buffer_size=1000000, replay_buffer_class=HerReplayBuffer, device="cuda:0",
                 # Parameters for HER
                 replay_buffer_kwargs=dict(n_sampled_goal=4, goal_selection_strategy="future"),
                 # Parameters for SAC
