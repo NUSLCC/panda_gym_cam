b5775a44749d29c8d018e74f99d8083c04c0f0a9
Chenchen Liu <chenchen.liu@u.nus.edu>
2024-02-07 16:27:56 +0800

    test with deform cnn with kinematics involved

--- panda_gym/utils.py
+++ panda_gym/utils.py
@@ -16,9 +16,28 @@ from stable_baselines3.common.type_aliases import TensorDict
 from typing import Dict
 from gymnasium import spaces
 import gymnasium as gym
-from timm import create_model
 from PIL import Image
 from torchvision.models import resnet18
+
+
+class FCN(BaseFeaturesExtractor):
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+    ) -> None:
+        super().__init__(observation_space, features_dim)
+        n_input_channels = observation_space.shape[0]
+        self.linear = nn.Sequential(
+            nn.Linear(n_input_channels, features_dim),
+            nn.ReLU(),
+            nn.Linear(features_dim, features_dim), 
+            nn.ReLU(),
+            nn.Linear(features_dim, features_dim), 
+            nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        return self.linear(observations) # use current frame
 
 
 class DeformableConv2d(nn.Module):
@@ -121,49 +140,6 @@ class DeformableCNNT(BaseFeaturesExtractor):
         return self.linear(self.deform_cnn(observations[:,-1,])) # use current frame
 
 
-class NatureCNN(BaseFeaturesExtractor):
-    """
-    :param observation_space:
-    :param features_dim: Number of features extracted.
-        This corresponds to the number of unit for the last layer.
-    :param normalized_image: Whether to assume that the image is already normalized
-        or not (this disables dtype and bounds checks): when True, it only checks that
-        the space is a Box and has 3 dimensions.
-        Otherwise, it checks that it has expected dtype (uint8) and bounds (values in [0, 255]).
-    """
-
-    def __init__(
-        self,
-        observation_space: gym.Space,
-        features_dim: int = 512,
-    ) -> None:
-        assert isinstance(observation_space, spaces.Box), (
-            "NatureCNN must be used with a gym.spaces.Box ",
-            f"observation space, not {observation_space}",
-        )
-        super().__init__(observation_space, features_dim)
-        # We assume CxHxW images (channels first)
-        n_input_channels = observation_space.shape[0] # last channel is feature channel from raw data [B, C H, W]
-        self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
-            nn.ReLU(),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
-            nn.ReLU(),
-            nn.Flatten(),
-        )
-
-        # Compute flatten shape by doing one forward pass
-        with torch.no_grad():
-            n_flatten = self.cnn(torch.as_tensor(observation_space.sample()[None]).float()).shape[1]
-        
-        self.linear = nn.Sequential(nn.Linear(n_flatten, features_dim), nn.ReLU())
-
-    def forward(self, observations: torch.Tensor) -> torch.Tensor:
-        return self.linear(self.cnn(observations))
-
-
 class NatureCNNT(BaseFeaturesExtractor):
     """
     :param observation_space:
@@ -181,7 +157,7 @@ class NatureCNNT(BaseFeaturesExtractor):
         features_dim: int = 512,
     ) -> None:
         assert isinstance(observation_space, spaces.Box), (
-            "NatureCNN must be used with a gym.spaces.Box ",
+            "NatureCNNT must be used with a gym.spaces.Box ",
             f"observation space, not {observation_space}",
         )
         super().__init__(observation_space, features_dim)
@@ -206,7 +182,66 @@ class NatureCNNT(BaseFeaturesExtractor):
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
         return self.linear(self.cnn(observations[:,-1,])) # use current frame
-    
+
+
+class DualCNNT(BaseFeaturesExtractor):
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+    ) -> None:
+        assert isinstance(observation_space, spaces.Box), (
+            "DualCNNT must be used with a gym.spaces.Box ",
+            f"observation space, not {observation_space}",
+        )
+        super().__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        n_input_channels = observation_space.shape[1] # [T, C, H, W]
+        each_network_channels = int(n_input_channels/2)
+
+        self.cnn1 = nn.Sequential(
+            nn.Conv2d(each_network_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        self.cnn2 = nn.Sequential(
+            nn.Conv2d(each_network_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        # Compute flatten shape by doing one forward pass
+        with torch.no_grad():
+            sample_array = observation_space.sample()[None][:,-1,]
+            channel_dim = sample_array.shape[1]
+            each_channel_dim = int(channel_dim/2)
+            sample_tensor1 = torch.tensor(sample_array[:, :each_channel_dim, :, :]).float()
+            sample_tensor2 = torch.tensor(sample_array[:, each_channel_dim:, :, :]).float()
+
+            n_flatten1 = self.cnn1(sample_tensor1).shape[1]
+            n_flatten2 = self.cnn2(sample_tensor2).shape[1]
+        
+        self.linear1 = nn.Sequential(nn.Linear(n_flatten1, int(features_dim/2)), nn.ReLU())
+        self.linear2 = nn.Sequential(nn.Linear(n_flatten2, int(features_dim/2)), nn.ReLU())
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        channel_dim = observations[:,-1,].shape[1]
+        each_channel_dim = int(channel_dim/2)
+        observations1 = observations[:,-1,][:, :each_channel_dim, :, :]
+        observations2 = observations[:,-1,][:, each_channel_dim:, :, :]
+        forward1 = self.linear1(self.cnn1(observations1))
+        forward2 = self.linear2(self.cnn2(observations2))
+        return torch.cat((forward1, forward2), dim=1)
+
 
 class CNNLSTM(BaseFeaturesExtractor):
     def __init__(
@@ -250,7 +285,7 @@ class CNNLSTM(BaseFeaturesExtractor):
         return x
 
 
-class CNNLSTM_6layers(BaseFeaturesExtractor):
+class CNNLSTM6Layers(BaseFeaturesExtractor):
     def __init__(
         self,
         observation_space: gym.Space,
@@ -264,17 +299,17 @@ class CNNLSTM_6layers(BaseFeaturesExtractor):
         # We assume CxHxW images (channels first)
         n_input_channels = observation_space.shape[1] # [T, C, H, W]
         self.cnn = nn.Sequential(
-            nn.Conv2d(n_input_channels, 32, kernel_size=6, stride=2, padding=2),
+            nn.Conv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
             nn.ReLU(),
-            nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=0),
             nn.ReLU(),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=2, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
             nn.ReLU(),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=0),
             nn.ReLU(),
             nn.Flatten(),
         )
@@ -298,6 +333,48 @@ class CNNLSTM_6layers(BaseFeaturesExtractor):
         return x
 
 
+class DeformCNNLSTM(BaseFeaturesExtractor):
+    def __init__(
+        self,
+        observation_space: gym.Space,
+        features_dim: int = 512,
+    ) -> None:
+        assert isinstance(observation_space, spaces.Box), (
+            "DeformCNNLSTM must be used with a gym.spaces.Box ",
+            f"observation space, not {observation_space}",
+        )
+        super().__init__(observation_space, features_dim)
+        # We assume CxHxW images (channels first)
+        n_input_channels = observation_space.shape[1] # [T, C, H, W]
+        self.deform_cnn = nn.Sequential(
+            DeformableConv2d(n_input_channels, 32, kernel_size=8, stride=4, padding=0),
+            nn.ReLU(),
+            DeformableConv2d(32, 64, kernel_size=4, stride=2, padding=0),
+            nn.ReLU(),
+            DeformableConv2d(64, 64, kernel_size=3, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+        with torch.no_grad():
+            each_T = observation_space.sample()[None][:,0,]
+            n_flatten = self.deform_cnn(torch.as_tensor(each_T).float()).shape[1]
+
+        self.fc_lstm = nn.Linear(n_flatten, features_dim)
+        self.lstm = nn.LSTM(input_size=features_dim, hidden_size=features_dim, num_layers=3)
+        self.fc1 = nn.Linear(features_dim, features_dim)
+
+    def forward(self, observations: torch.Tensor) -> torch.Tensor:
+        hidden = None
+        for t in range(observations.size(1)):
+            x = self.deform_cnn(observations[:, t, :, :, :]) 
+            x = self.fc_lstm(x)
+            out, hidden = self.lstm(x.unsqueeze(0), hidden)         
+
+        x = self.fc1(out[-1, :, :])
+        x = F.relu(x)
+        return x
+
+
 class Resnet18LSTM(BaseFeaturesExtractor):
     def __init__(
         self,
@@ -312,20 +389,17 @@ class Resnet18LSTM(BaseFeaturesExtractor):
         n_input_channels = observation_space.shape[1] # [T, C, H, W]
         resnet = resnet18(pretrained=False)
         down_blocks = []
-        
         self.input_block = nn.Sequential(
             nn.Conv2d(6, 64, kernel_size=7, stride=1, padding=3),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True)
             
         )
-        self.pooling = list(resnet.children())[3]
         for bottleneck in list(resnet.children()):
             if isinstance(bottleneck, nn.Sequential):
                 down_blocks.append(bottleneck)
         self.down_blocks = nn.Sequential(*down_blocks)
         self.resnet = nn.Sequential(self.input_block,
-                                    self.pooling,
                                     self.down_blocks,
                                     nn.Flatten()
                                     )
@@ -335,7 +409,7 @@ class Resnet18LSTM(BaseFeaturesExtractor):
             n_flatten = self.resnet(torch.as_tensor(each_T).float()).shape[1]
 
         self.fc_lstm = nn.Linear(n_flatten, features_dim)
-        self.lstm = nn.LSTM(input_size=features_dim, hidden_size=features_dim, num_layers=3)
+        # self.lstm = nn.LSTM(input_size=features_dim, hidden_size=features_dim, num_layers=3)
         self.fc1 = nn.Linear(features_dim, features_dim)
 
     def forward(self, observations: torch.Tensor) -> torch.Tensor:
