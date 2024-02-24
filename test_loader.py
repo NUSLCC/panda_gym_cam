aa9d5aa421328b053c6476a89863c614c0be5b29
Chenchen Liu <chenchen.liu@u.nus.edu>
2024-02-24 22:01:53 +0800

    add test_loader to test zig traj based on sine traj model

--- test_loader.py
+++ test_loader.py
@@ -1,26 +1,53 @@
 from panda_gym.envs import PandaReachCamEnv
-from stable_baselines3 import DDPG, HerReplayBuffer
-from sb3_contrib import TQC
-import time
+from stable_baselines3 import SAC, HerReplayBuffer
 import gymnasium as gym
+from panda_gym.utils import CustomCombinedExtractor
 
-env = gym.make('PandaReachCam-v3', render_mode="human", control_type="ee") # rgb_array
-print(env.action_space)
-print(env.observation_space)
+env = gym.make('PandaReachCam-v3', render_mode="human", control_type="ee")
+# print(env.action_space)
+# print(env.observation_space)
 # HER must be loaded with the env
-model = DDPG.load("ddpg_her_panda_ee_model", env=env)
 
+# Remember to change the feature extractors in utils
+# model_name = "sac_rgb_randbig_moving_lstm_ee_kine_model"
+model_name = "sac_rgb_randbig_moving_deformcnn_ee_kine_model"
+# model_name = "sac_rgb_randbig_moving_dualcnn_ee_kine_model"
+# model_name = "sac_rgb_randbig_moving_naturecnn_ee_kine_model"
+
+model = SAC.load(model_name, env=env)
 obs, _ = env.reset()
-for i in range(1000):
+
+success_count = 0
+failure_count = 0
+term_count = 0
+trun_count = 0
+
+while True:
     action, _states = model.predict(obs, deterministic=True)
-    print(action)
     obs, reward, terminated, truncated, info = env.step(action)
     env.render()
     if terminated:
-      print("terminated")
-      print(i)
+      term_count += 1
+      print("Current Episode No.", term_count + trun_count)
+      if info["is_success"] == True and info["is_failure"] == False:
+         success_count += 1
+      elif info["is_success"] == False and info["is_failure"] == True:
+         failure_count += 1
       obs, info = env.reset()
+
     elif truncated:
-      print("truncated")
-      print(info)
+      trun_count += 1
+      print("truncated info:", info)
+      print("Current Episode No.", term_count + trun_count)
       obs, info = env.reset()
+    
+    if (term_count + trun_count) == 100:
+      break
+
+print("******************************************************************")
+print("Model name:", model_name)
+print("success count:", success_count)
+print("failure count:", failure_count)
+print("terminate count:", term_count)
+print("truncated count:", trun_count)
+print("******************************************************************")
