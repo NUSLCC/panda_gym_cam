2a95755e672e75541b2f86b7774a40e02428dbfa
philiprxwang <philipruoxuan.wang@yahoo.com>
2024-04-24 20:08:30 +0800

    modify the loader

--- test_loader.py
+++ test_loader.py
@@ -19,10 +19,12 @@ env = gym.make('PandaReachCam-v3', render_mode="human", control_type="ee")
 # model_name = "sac_rgb_randbig_moving_deformcnn_ee_kine_model"
 
 # model_name = "sac_rgb_randbig_moving_dualcnn_ee_model"
-model_name = "sac_rgb_randbig_moving_dualcnn_ee_kine_model"
+# model_name = "sac_rgb_randbig_moving_dualcnn_ee_kine_model"
 
 # model_name = "sac_rgb_randbig_moving_naturecnn_ee_model"
 # model_name = "sac_rgb_randbig_moving_naturecnn_ee_kine_model"
+
+model_name = "sac_rgb_randzig_moving_lstm_ee_kine_model"
 
 model = SAC.load(model_name, env=env)
 obs, _ = env.reset()
